c9ea9a56bb5494f65eae66e9d84baa4706062beb
stopdaydreaming <hello@stopdaydreaming.com>
2021-01-14 11:45:51 -0500

    updated server port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.end.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -90,5 +90,5 @@ app.post("/api/characters", function(req, res) {
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT http://localhost:" + PORT);
 });
